11115f0b005352fe214921130241869fa36e91cb
Marco Wittig <Marco-Wittig@gmx.de>
2022-08-22 13:52:12 +0200

    Refactor player effects

--- src/main/java/io/github/mwttg/games/platform/player/effect/PlayerEffect.java
+++ src/main/java/io/github/mwttg/games/platform/player/effect/PlayerEffect.java
@@ -2,9 +2,9 @@ package io.github.mwttg.games.platform.player.effect;
 
 import org.joml.Matrix4f;
 
-public record PlayerEffect(String name, Matrix4f transform, long createTimestamp, int lifetime) {
+public record PlayerEffect(String animationName, Matrix4f transform, long createTimestamp, int lifetime) {
 
-  public boolean isDead(final long currentTimestamp) {
-    return currentTimestamp > createTimestamp + lifetime;
+  public boolean isDead() {
+    return System.currentTimeMillis() > createTimestamp + lifetime;
   }
 }
--- src/main/java/io/github/mwttg/games/platform/player/effect/PlayerEffectSystem.java
+++ src/main/java/io/github/mwttg/games/platform/player/effect/PlayerEffectSystem.java
@@ -2,7 +2,6 @@ package io.github.mwttg.games.platform.player.effect;
 
 import io.github.mwttg.games.platform.draw.SpriteAnimationSystem;
 import java.util.ArrayList;
-import java.util.List;
 import org.joml.Matrix4f;
 
 public class PlayerEffectSystem {
@@ -12,31 +11,20 @@ public class PlayerEffectSystem {
 
   public static void draw(final PlayerEffectComponent playerEffectComponent, final Matrix4f view, final Matrix4f projection) {
     for (final PlayerEffect effect : playerEffectComponent.getEffects()) {
-      final var animation = playerEffectComponent.getAnimationComponentByName().get(effect.name());
+      final var animation = playerEffectComponent.getAnimationComponentByName().get(effect.animationName());
       SpriteAnimationSystem.draw(animation, effect.transform(), view, projection);
     }
   }
 
   public static void update(final PlayerEffectComponent playerEffectComponent) {
-    final var current = System.currentTimeMillis();
-    final var effects = playerEffectComponent.getEffects();
-    final var indexToRemove = new ArrayList<PlayerEffect>();
-    for (final PlayerEffect effect : effects) {
-      if (effect.isDead(current)) {
-        indexToRemove.add(effect);
+    final var updated = new ArrayList<PlayerEffect>();
+
+    for (final PlayerEffect effect : playerEffectComponent.getEffects()) {
+      if (!effect.isDead()) {
+        updated.add(effect);
       }
     }
 
-    final var effectsAlive = remove(effects, indexToRemove);
-    playerEffectComponent.updateAliveEffects(effectsAlive);
-  }
-
-  private static List<PlayerEffect> remove(final List<PlayerEffect> source, final List<PlayerEffect> removables) {
-
-    final var result = new ArrayList<>(source);
-    for (final PlayerEffect effect : removables) {
-      result.remove(effect);
-    }
-    return result;
+    playerEffectComponent.updateAliveEffects(updated);
   }
 }

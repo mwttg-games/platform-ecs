2439f20049ea7610f3372489822c9f4730d57910
Marco Wittig <Marco-Wittig@gmx.de>
2022-09-27 18:10:09 +0200

    Refactor Player animation definition (loader)
    now it's possible to link a static Sprite (no animation) to a player state

--- src/main/java/io/github/mwttg/games/platform/player/PlayerDefinition.java
+++ src/main/java/io/github/mwttg/games/platform/player/PlayerDefinition.java
@@ -5,6 +5,7 @@ import io.github.mwttg.games.opengl.basic.utilities.geometry.MeshFactory;
 import io.github.mwttg.games.opengl.basic.utilities.texture.Texture;
 import io.github.mwttg.games.platform.draw.AnimatedSprite;
 import io.github.mwttg.games.platform.draw.Drawable;
+import io.github.mwttg.games.platform.draw.Sprite;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,17 +27,22 @@ public record PlayerDefinition(TileSize tileSize, Map<String, AnimationDefinitio
     for (final Map.Entry<String, AnimationDefinition> entry : definition.animations.entrySet()) {
       final var key = entry.getKey();
       final var value = entry.getValue();
-
-      if (value.frames() != value.timings().size()) {
-        throw new RuntimeException("player definition file is wrong (frames != timings (count))");
-      }
-
-      final var plane = MeshFactory.createAnimatedSprite(value.frames(), tileSize.width(), tileSize.height());
       final var textureId = Texture.create(value.spriteSheet());
-      final var component = AnimatedSprite.create(plane.geometry(),
-          plane.textureCoordinates(),
-          textureId, value.timings());
-      result.put(key, component);
+
+      if (value.frames == 0) {
+        final var plane = MeshFactory.createSprite(tileSize.width(), tileSize.height());
+        final var sprite = Sprite.create(plane.geometry(), plane.textureCoordinates(), textureId);
+        result.put(key, sprite);
+      } else {
+
+        if (value.frames() != value.timings().size()) {
+          throw new RuntimeException("player definition file is wrong (frames != timings (count))");
+        }
+
+        final var plane = MeshFactory.createAnimatedSprite(value.frames(), tileSize.width(), tileSize.height());
+        final var animation = AnimatedSprite.create(plane.geometry(), plane.textureCoordinates(), textureId, value.timings());
+        result.put(key, animation);
+      }
     }
 
     return result;
